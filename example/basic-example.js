ad8ce80efbc32b45ef34d3986a7cc8f21be12b8d
Kalman Keri <kk@metamaya.org>
2016-08-20 18:43:03 +0200

    example: new examples added

---
+++ example/basic-example.js
@@ -0,0 +1,5 @@
+﻿var mm = require('metamaya')
+var printer = require('metamaya-printer').printer
+var example = mm.compile('f(x) = x * x; a = f(5)')
+printer.text("model: ").model(example).br()
+printer.text("a = ").model(example.get('a').value()).br()
